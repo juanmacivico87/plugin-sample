45f1046976fc1087fe0d8956dffe894e26a3264c
juanmacivico87 <juanmacivico87@gmail.com>
2020-05-26 20:00:45 +0200

    Replace wp_die to admin_notices in activation and deactivation hooks

--- config/PluginConfig.php
+++ config/PluginConfig.php
@@ -8,6 +8,10 @@ class JMC87_PluginConfig
     {
         add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
         add_action( 'wp_enqueue_scripts', array( $this, 'load_includes' ) );
+
+        add_action( 'admin_init', array( $this, 'activate_plugin_process' ) );
+        add_action( 'admin_init', array( $this, 'deactivate_plugin_process' ) );
+        add_action( 'admin_init', array( $this, 'uninstall_plugin_process' ) );
     }
 
     public function load_textdomain()
@@ -27,21 +31,66 @@ class JMC87_PluginConfig
     public static function plugin_install()
     {
         if ( !current_user_can( 'activate_plugins' ) ) {
-            wp_die( sprintf( __( 'Don\'t have enough permissions to install this plugin. <a href="%s">Back to plugins page.</a>', 'plugin-textdomain' ), esc_url( admin_url( 'plugins.php' ) ) ) );
+            add_option( '__prefix_activate_plugin', 'plugin-sample' );
         }
+    }
+
+    public function activate_plugin_process()
+    {
+        if ( is_admin() && get_option( '__prefix_activate_plugin' ) === 'plugin-sample' ) {
+            delete_option( '__prefix_activate_plugin' );
+            deactivate_plugins( 'plugin-sample/plugin-sample.php', true );
+            add_action( 'admin_notices', array( $this, 'render_activate_plugin_notice' ) );
+        }
+    }
+
+    public function render_activate_plugin_notice()
+    {
+        ?><div class="notice notice-error is-dismissible">
+            <p><?php _e( 'Don\'t have enough permissions to install this plugin.', 'plugin-textdomain' ); ?></p>
+        </div><?php
     }
 
     public static function plugin_deactivate()
     {
         if ( !current_user_can( 'activate_plugins' ) ) {
-            wp_die( sprintf( __( 'Don\'t have enough permissions to deactivate this plugin. <a href="%s">Back to plugins page.</a>', 'plugin-textdomain' ), esc_url( admin_url( 'plugins.php' ) ) ) );
+            add_option( '__prefix_not_deactivate_plugin', 'plugin-sample' );
+            wp_redirect( admin_url( 'plugins.php' ) );
+            die();
         }
+    }
+
+    public function deactivate_plugin_process()
+    {
+        if ( is_admin() && get_option( '__prefix_not_deactivate_plugin' ) === 'plugin-sample' ) {
+            delete_option( '__prefix_not_deactivate_plugin' );
+            add_action( 'admin_notices', array( $this, 'render_deactivate_plugin_notice' ) );
+        }
+    }
+
+    public function render_deactivate_plugin_notice()
+    {
+        ?><div class="notice notice-error is-dismissible">
+            <p><?php _e( 'Don\'t have enough permissions to deactivate this plugin.', 'plugin-textdomain' ); ?></p>
+        </div><?php
     }
 
     public static function plugin_uninstall()
     {
-        if ( !current_user_can( 'activate_plugins' ) ) {
-            wp_die( sprintf( __( 'Don\'t have enough permissions to uninstall this plugin. <a href="%s">Back to plugins page.</a>', 'plugin-textdomain' ), esc_url( admin_url( 'plugins.php' ) ) ) );
+        if ( current_user_can( 'activate_plugins' ) ) {
+            
         }
+    }
+
+    public function uninstall_plugin_process()
+    {
+        
+    }
+
+    public function render_uninstall_plugin_notice()
+    {
+        ?><div class="notice notice-error is-dismissible">
+            <p><?php _e( 'Don\'t have enough permissions to uninstall this plugin.', 'plugin-textdomain' ); ?></p>
+        </div><?php
     }
 }
