e3a21b484cec654e7c9000ba3da7bd528408c1d0
juanmacivico87 <juanmacivico87@gmail.com>
2021-03-01 14:02:38 +0100

    Add PHPDoc comments and refactor CustomRestField class

--- src/RestApi/CustomRestField/CustomRestField.php
+++ src/RestApi/CustomRestField/CustomRestField.php
@@ -1,27 +1,67 @@
 <?php
-/**
- * @package plugin-sample
- */
-
 namespace PrefixSource\RestApi\CustomRestField;
 
 if ( !defined( 'ABSPATH' ) )
     exit;
 
+/**
+ * CustomRestField
+ *
+ * This class provides an example to add a new field with information to an endpoint already defined in the
+ * WordPress Rest API. For more information, visit the @link https://developer.wordpress.org/reference/functions/register_rest_field/
+ *
+ * @version	1.0
+ * @since  	1.0
+ * @package	plugin-sample
+ */
 class CustomRestField
 {
     private $post_types = array( 'post', 'page' );
 
+    /**
+     * __construct()
+     *
+     * This method is responsible for initializing the class and assigning values to its internal properties, from anywhere
+     * in the code where an object of that class is instantiated.
+     *
+     * @return 	void
+     * @access 	public
+     * @version	1.0
+     * @since  	1.0
+     * @package	plugin-sample
+     */
     public function __construct()
     {
         $this->init();
     }
 
+    /**
+     * init()
+     *
+     * This method takes care of hooking the rest of the methods of the class in the corresponding hooks that are provided for it.
+     *
+     * @return 	void
+     * @access 	public
+     * @version	1.0
+     * @since  	1.0
+     * @package	plugin-sample
+     */
     public function init()
     {
         add_action( 'rest_api_init', array( $this, 'add_new_rest_field' ) );
     }
 
+    /**
+     * add_new_rest_field()
+     *
+     * This method is responsible for creating the new field and adding it to the indicated endpoint.
+     *
+     * @return 	void
+     * @access 	public
+     * @version	1.0
+     * @since  	1.0
+     * @package	plugin-sample
+     */
     public function add_new_rest_field()
     {
         $args = array(
@@ -31,6 +71,17 @@ class CustomRestField
         register_rest_field( $this->post_types, 'custom-rest-field', $args );
     }
 
+    /**
+     * get_rest_field_value()
+     *
+     * This method takes care of returning the value of the new field from the endpoint.
+     *
+     * @return 	string  Value of the new field
+     * @access 	public
+     * @version	1.0
+     * @since  	1.0
+     * @package	plugin-sample
+     */
     public function get_rest_field_value()
     {
         return 'Some value';
