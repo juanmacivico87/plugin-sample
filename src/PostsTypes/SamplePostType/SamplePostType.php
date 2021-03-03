2f1deb6a19cc404989ae6502fa697215834134d6
juanmacivico87 <juanmacivico87@gmail.com>
2021-03-03 12:40:01 +0100

    Add PHPDoc comments and refactor SamplePostType class

--- src/PostsTypes/SamplePostType/SamplePostType.php
+++ src/PostsTypes/SamplePostType/SamplePostType.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * A snippet to create a new custom post type in WordPress. For more info, view:
- *
- * @link https://codex.wordpress.org/Function_Reference/register_post_type
- *
- * @package plugin-sample
- */
-
 namespace PrefixSource\PostsTypes\SamplePostType;
 
 if ( !defined( 'ABSPATH' ) )
@@ -14,6 +6,16 @@ if ( !defined( 'ABSPATH' ) )
 
 use PrefixSource\Taxonomies\CustomCategory\CustomCategory;
 
+/**
+ * SamplePostType
+ *
+ * This class provides an example to create a new custom post type in WordPress.
+ * For more information, visit the @link https://codex.wordpress.org/Function_Reference/register_post_type
+ *
+ * @version	1.0
+ * @since  	1.0
+ * @package	plugin-sample
+ */
 class SamplePostType
 {
     const POST_TYPE_NAME    = 'sample';
@@ -28,11 +30,34 @@ class SamplePostType
         'pages'      => true,
     );
 
+    /**
+     * __construct()
+     *
+     * This method is responsible for initializing the class and assigning values to its internal properties, from anywhere
+     * in the code where an object of that class is instantiated.
+     *
+     * @return 	void
+     * @access 	public
+     * @version	1.0
+     * @since  	1.0
+     * @package	plugin-sample
+     */
     public function __construct()
     {
         $this->init();
     }
 
+    /**
+     * init()
+     *
+     * This method takes care of hooking the rest of the methods of the class in the corresponding hooks that are provided for it.
+     *
+     * @return 	void
+     * @access 	public
+     * @version	1.0
+     * @since  	1.0
+     * @package	plugin-sample
+     */
     public function init()
     {
         add_action( 'init', array( $this, 'set_custom_post_type_slug' ), 5 );
@@ -41,11 +66,33 @@ class SamplePostType
         add_filter( 'single_template', array( $this, 'get_post_type_templates' ) );
     }
 
+    /**
+     * set_custom_post_type_slug()
+     *
+     * This method is responsible for setting the Custom Post Type slug for each of the languages in which the plugin is translated.
+     *
+     * @return 	void
+     * @access 	public
+     * @version	1.0
+     * @since  	1.0
+     * @package	plugin-sample
+     */
     public function set_custom_post_type_slug()
     {
         $this->rewrite['slug'] = __( 'samples', 'plugin-sample' );
     }
 
+    /**
+     * add_custom_post_type()
+     *
+     * This method takes care of registering the new Custom Post Type in WordPress.
+     *
+     * @return 	void
+     * @access 	public
+     * @version	1.0
+     * @since  	1.0
+     * @package	plugin-sample
+     */
     public function add_custom_post_type()
     {
         $args = array(
@@ -123,24 +170,46 @@ class SamplePostType
         flush_rewrite_rules();
     }
 
+    /**
+     * get_post_type_templates()
+     *
+     * This method is responsible for setting the templates for the Custom Post Type file and single, from the plugin itself.
+     *
+     * @param   string  $template Path to the template
+     * @return 	string  New path to the template
+     * @access 	public
+     * @version	1.0
+     * @since  	1.0
+     * @package	plugin-sample
+     */
     public function get_post_type_templates( $template )
     {
-        if ( get_post_type() === self::POST_TYPE_NAME )
-        { 
-            switch( true ) 
-            {
-                case is_archive() && !is_tax():
-                    $template = PREFIX_PLUGIN_DIR . 'src/PostsTypes/SamplePostType/views/archive-sample.php';
-                    break;
-                case is_single():
-                    $template = PREFIX_PLUGIN_DIR . 'src/PostsTypes/SamplePostType/views/single-sample.php';
-                    break;
-            }
+        if ( get_post_type() !== self::POST_TYPE_NAME )
+            return $template;
+
+        switch( true ) {
+            case is_archive() && !is_tax():
+                $template = PREFIX_PLUGIN_DIR . 'src/PostsTypes/SamplePostType/views/archive-sample.php';
+                break;
+            case is_single():
+                $template = PREFIX_PLUGIN_DIR . 'src/PostsTypes/SamplePostType/views/single-sample.php';
+                break;
         }
 
         return $template;
     }
 
+    /**
+     * set_roles_capabilities()
+     *
+     * This method takes care of assigning the different capabilities to each of the roles that have been defined on the web.
+     *
+     * @return 	void
+     * @access 	public
+     * @version	1.0
+     * @since  	1.0
+     * @package	plugin-sample
+     */
     public static function set_roles_capabilities()
     {
         global $wp_roles;
