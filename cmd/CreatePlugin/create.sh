11d6c5fce794019e04f4a2e6f15ef996357b06b4
juanmacivico87 <juanmacivico87@gmail.com>
2021-05-03 09:36:15 +0200

    Fix create-plugin command and add blank spaces in create.sh file

--- cmd/CreatePlugin/create.sh
+++ cmd/CreatePlugin/create.sh
@@ -11,36 +11,42 @@ echo ""
 echo -e "\033[1;32m Please, enter a name for your new plugin (for example: 'My Awesome Plugin') \033[0m"
 echo ""
 echo -e "\033[1;31m Remember that if the name of the plugin contains more than one word, it must be enclosed in single quotes \033[0m"
+echo ""
 read PLUGIN_NAME
 
 echo ""
 echo -e "\033[1;32m Now, enter a description for your new plugin (for example: 'This plugin allows you to live an awesome experience') \033[0m"
 echo ""
 echo -e "\033[1;31m Remember that if the description of the plugin contains more than one word, it must be enclosed in single quotes \033[0m"
+echo ""
 read PLUGIN_DESCRIPTION
 
 echo ""
 echo -e "\033[1;32m Enter a slug for your new plugin (for example: 'my-awesome-plugin') \033[0m"
 echo ""
 echo -e "\033[1;31m Remember that it should always be lower case and separate the words by hyphens \033[0m"
+echo ""
 read PLUGIN_SLUG
 
 echo ""
 echo -e "\033[1;32m Enter the URI of the plugin (for example: 'https:\/\/www.myawesomeplugin.com') \033[0m"
 echo ""
 echo -e "\033[1;31m Remember that you must enclose the URI in single quotes and for each slash (/), use a backslash (\). Otherwise the URI will not be set correctly in the headers of the plugin \033[0m"
+echo ""
 read PLUGIN_URI
 
 echo ""
 echo -e "\033[1;32m It's time for everyone to know who has developed this plugin, so enter your name or your brand name \033[0m"
 echo ""
 echo -e "\033[1;31m Remember that if your name contains more than one word, it must be enclosed in single quotes \033[0m"
+echo ""
 read PLUGIN_AUTHOR
 
 echo ""
 echo -e "\033[1;32m Enter the URL of your website (for example: 'https:\/\/www.juanmacivico87.com') \033[0m"
 echo ""
 echo -e "\033[1;31m Remember that you must enclose the URI in single quotes and for each slash (/), use a backslash (\). Otherwise the URI will not be set correctly in the headers of the plugin \033[0m"
+echo ""
 read PLUGIN_AUTHOR_URI
 
 echo ""
@@ -50,24 +56,28 @@ echo ""
 echo -e "\033[1;32m First, enter the name of the composer vendor (for example: my-vendor-name). This information will be set in the composer.json file of your plugin \033[0m"
 echo ""
 echo -e "\033[1;31m Remember that it should always be lower case and separate the words by hyphens \033[0m"
+echo ""
 read COMPOSER_VENDOR_NAME
 
 echo ""
 echo -e "\033[1;32m Now comes the turn of the namespaces for the classes of your plugin. We start with the one in the configuration directory (for example: MyAwesomeConfig) \033[0m"
 echo ""
 echo -e "\033[1;31m Remember that the namespace must not be separated by spaces and that an UpperCamelCase structure must be used to define it \033[0m"
+echo ""
 read PLUGIN_CONFIG_NAMESPACE
 
 echo ""
 echo -e "\033[1;32m We will continue with the namespace of the classes in the source directory (for example: MyAwesomeSrc) \033[0m"
 echo ""
 echo -e "\033[1;31m Remember that the namespace must not be separated by spaces and that an UpperCamelCase structure must be used to define it \033[0m"
+echo ""
 read PLUGIN_SOURCE_NAMESPACE
 
 echo ""
 echo -e "\033[1;32m And finally, enter the namespace of the command classes (for example: MyAwesomeCmd). In this way, you can also write your own commands if you need it \033[0m"
 echo ""
 echo -e "\033[1;31m Remember that the namespace must not be separated by spaces and that an UpperCamelCase structure must be used to define it \033[0m"
+echo ""
 read PLUGIN_COMMAND_NAMESPACE
 
 echo ""
@@ -77,12 +87,14 @@ echo ""
 echo -e "\033[1;32m Enter the prefix for functions and variables (for example: my_awesome_) \033[0m"
 echo ""
 echo -e "\033[1;31m Remember that it should always be lower case and separate the words by underscores \033[0m"
+echo ""
 read PLUGIN_VARS_PREFIX
 
 echo ""
 echo -e "\033[1;32m Enter the prefix for constants (for example: MY_AWESOME_) \033[0m"
 echo ""
 echo -e "\033[1;31m Remember that it should always be upper case and separate the words by underscores \033[0m"
+echo ""
 read PLUGIN_CONSTANTS_PREFIX
 
 echo ""
